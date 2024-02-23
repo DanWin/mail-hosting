3f6cfe7c14b58ff05017edcd197bd33e418e5efc
Daniel Winzen <daniel@danwin1210.de>
2024-02-23 20:42:02 +0100

    Add more cleanups

--- tools/delete_leftover_files.php
+++ tools/delete_leftover_files.php
@@ -25,3 +25,36 @@ foreach ( $domains as $domain ) {
 		}
 	}
 }
+$domains = array_diff( scandir( '/var/local/snappymail/_data_/_default_/storage/' ), array( '..', '.', '__nobody__' ) );
+$dirs = [];
+foreach ( $domains as $domain ) {
+	if ( is_dir( '/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) ) ) {
+		if ( ! isset( $mailboxes[ $domain ] ) ) {
+			echo sprintf(_('%s does not seem to have any accounts, but has a snappymail directory. Consider deleting it.'), $domain).PHP_EOL;
+		} else {
+			$accounts = array_diff( scandir( '/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) ), array( '..', '.' ) );
+			foreach ( $accounts as $account ) {
+				if ( ! isset( $mailboxes[ $domain ][ $account ] ) && is_dir( '/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) . '/' . basename( $account ) ) ) {
+					exec( 'rm -r ' . escapeshellarg('/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) . '/' . basename( $account )));
+					echo sprintf(_('Deleted: %s'), '/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) . '/' . basename( $account )) . PHP_EOL;
+				} elseif( is_file('/var/local/snappymail/_data_/_default_/storage/' . basename( $domain ) . '/' . basename( $account ))){
+					echo sprintf(_('File found in mail directory location: "%s". Consider deleting it.'), '/var/local/snappymail/_data_/_default_/storage/'. basename( $domain ) . '/' . basename( $account ) ) . PHP_EOL;
+				}
+			}
+		}
+	}
+}
+$accout_files = array_diff( scandir( '/var/local/squirrelmail/data/' ), array( '..', '.' ) );
+foreach( $accout_files as $file ){
+	if(preg_match( '/^(.+?)(@(.+))?\.(pref|abook|sig)$/', $file, $matches )){
+		$domain = $matches[ 3 ];
+		if(in_array($domain, ['', 'danielas3rtn54uwmofdo3x2bsdifr47huasnmbgqzfrec5ubupvtpid.onion'], true)){
+			$domain = 'danwin1210.de';
+		}
+		$account = $matches[ 1 ];
+		if ( ! isset( $mailboxes[ $domain ][ $account ] )){
+			unlink('/var/local/squirrelmail/data/' . $file);
+			echo sprintf(_('Deleted: %s'), '/var/local/squirrelmail/data/' . $file) . PHP_EOL;
+		}
+	}
+}
