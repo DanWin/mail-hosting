3f067b1f9230f53899c18dfaa533e1fbc55277d2
DEAM0 <45461292+DEAM0@users.noreply.github.com>
2024-06-11 12:16:41 +0200

    Update install_binaries_proxy.sh
    Missing Brotli, proxy server not start after restart

--- install_binaries_proxy.sh
+++ install_binaries_proxy.sh
@@ -5,7 +5,7 @@ export LANG=C.UTF-8
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 # install all required packages
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends install -y bash-completion bind9 ca-certificates coturn curl git gnupg haveged iptables libnginx-mod-stream libsasl2-modules logrotate lsb-release nano nginx openssl postfix postfix-mysql postfix-mta-sts-resolver rng-tools5 vim wget wireguard wireguard-tools
+DEBIAN_FRONTEND=noninteractive apt-get --no-install-recommends install -y bash-completion bind9 ca-certificates coturn curl git gnupg haveged iptables libnginx-mod-stream libnginx-mod-http-brotli-filter libsasl2-modules logrotate lsb-release nano nginx openssl postfix postfix-mysql postfix-mta-sts-resolver rng-tools5 vim wget wireguard wireguard-tools
 
 # dhparams
 for file in /etc/nginx/dh4096.pem; do
